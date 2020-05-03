ccb8d4e5d747927d07c1b255653c2e86f97f895b
Amisha <pamisha22@yahoo.com>
2020-05-03 02:30:33 -0400

    js-sport-game update1

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+
+let Team1
+shootButton = document.querySelector("#teamone-shoot-button");
+shootButton.addEventListener("click", function () {
+
+    console.log("team1_goal");
+    let newteam1_goal = Number("teamone - numgoals".innerHTML) + 1;
+    ("teamone-numgoals".innerHTML) = newteam1_goal;
+
+})
+let team2
+shootButton = document.querySelector("#teamtwo-shoot-button");
+shootButton.addEventListener("click", function () {
+
+    console.log("team2_goal")
+    let team2_goal = Number("teamtwo - numgoals".innerHTML) + 1;
+    ("teamtwo-numgoals".innerHTML) = "team2_goal";
+})
+
+/*function getRandomshoot(0 ,1) {
+    return Math.random() * (5 - 1) + 1;*/
+let
+    shotstakenButton = document.querySelector("#teamone-numshots");
+
+if (console.log(Math.random() * (5 - 1) + 1)); (console.log("success"));
+
+let
+    shotstakenButton2 = document.querySelector("#teamtwo-numshots");
+
+if (console.log(Math.random() * (5 - 1) + 1));
+(console.log("fail"));
+
+
+let count = 0;
+const resets = document.querySelector("#num-reset");
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", function () {
+
+
+    if (console.log("team1_goal"));
+    let team1_goal = Number("teamone- numgoals".innerHTML) * 0;
+    ("teamone-numgoals".innerHTML) = "team1_goal";
+
+    if (console.log("team2_goal"));
+    let team2_goal = Number("teamtwo - numgoals".innerHTML) * 0;
+    ("teamtwo-numgoals".innerHTML) = "team2_goal";
+
+
+
+})
+
+
+
+
